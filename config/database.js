5bcd09766bab9c325f6447249ba74a5c9e91f8d6
inigo96 <inigo.alon@opendeusto.es>
2017-12-31 01:09:47 +0100

    working with mongodb

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@
 module.exports = {
 
 //    'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'url' : 'mongodb://ds133597.mlab.com:33597/heroku_f585v9lk/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://ds133597.mlab.com:33597/heroku_f585v9lk' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
